9385a53d0a3e936906d219d1aaa4fa8c8f20df94
Vitor Franco do Carmo <vfcarmo@gmail.com>
2020-09-16 22:41:29 -0300

    Add console log to see the class lifecicle

--- src/components/ClassComponent/ClassComponent.tsx
+++ src/components/ClassComponent/ClassComponent.tsx
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
 
 class ClassComponent extends Component<{ name: string }> {
+  constructor(props: any) {
+    super(props);
+    console.log("constructor reached");
+  }
+
   state = {
-    name: "Mundo!!!",
+    name: "Mundo!!!!",
   };
+
+  componentDidMount() {
+    console.log("DidMount reached");
+  }
+
+  componentDidUpdate() {
+    console.log("DidUpdate reached");
+  }
+
   render() {
+    console.log("render reached");
     return (
       <div>
         <p>Name: {this.state.name}</p>
