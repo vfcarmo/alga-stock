710a84ca1916140d1c59af9b2b7b2c5c6ddfd38a
Vitor Franco do Carmo <vfcarmo@gmail.com>
2020-09-17 00:43:04 -0300

    clear whole application

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,27 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import TestComponent from "../TestComponent";
-import ClassComponent from "../ClassComponent/ClassComponent";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TestComponent name="Mundo" />
-        {/* <ClassComponent name="Mundo" /> */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">App</div>;
 }
 
 export default App;
