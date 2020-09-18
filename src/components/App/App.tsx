cc96ce92fb4d84c23ee1bc7727e4ebb3293c589e
Vitor Franco do Carmo <vfcarmo@gmail.com>
2020-09-18 00:42:03 -0300

    clear app + add table component's base file

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,47 +1,14 @@
 import React, { useState } from "react";
 import "./App.css";
 import Header from "../Header";
-import Button from "../../shared/Button";
 import Container from "../../shared/Container";
-import Input from "../../shared/Input";
-
-function TestComponent() {
-  return (
-    <img
-      src="https://img.icons8.com/color/search"
-      alt="Search icon"
-      width="16px"
-    />
-  );
-}
 
 function App() {
   const [name, setName] = useState("");
   return (
     <div className="App">
       <Header title="AlgaStock" />
-      <Container>
-        <Input
-          type="text"
-          name="name"
-          label="Name"
-          placeholder="Enter your name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <ul>
-          {["Vitor", "Karen", "Mariana"].map((name, index) => {
-            return <li key={index}>{name}</li>;
-          })}
-        </ul>
-        <Button
-          onClick={() => window.alert(name)}
-          appendIcon={<TestComponent />}
-        >
-          Alert
-        </Button>
-      </Container>
-      App
+      <Container></Container>
     </div>
   );
 }
