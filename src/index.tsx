2482a6d36566673aec3822940d8b668e456c794d
Vitor Franco do Carmo <vfcarmo@gmail.com>
2020-09-22 13:26:51 -0300

    add axios to project

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App/";
+import http from './utils/http'
 
 ReactDOM.render(
   <React.StrictMode>
