8ff892d5307e518fdd8269ead27d5b037d5b0e30
Vitor Franco do Carmo <vfcarmo@gmail.com>
2020-09-21 21:09:08 -0300

    form reutilization

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import Header from "../Header";
 import Container from "../../shared/Container";
 import Table, { TableHeader } from "../../shared/Table";
-import Products from "../../shared/Table/Table.mockdata";
+import Products, { Product } from "../../shared/Table/Table.mockdata";
 import ProductForm, { ProductCreator } from "../Products/ProductForm";
 
 const headers: TableHeader[] = [
@@ -27,12 +27,19 @@ function App() {
     ])
   }
 
+  const handleProductUpdate = (newProduct: Product) => {
+    setProducts(products.map(product => product.id === newProduct.id ? newProduct : product))
+  }
+
   return (
     <div className="App">
       <Header title="AlgaStock" />
       <Container>
         <Table headers={headers} data={products} />
-        <ProductForm onSubmit={handleProductSubmit} />
+        <ProductForm form={products[2]}
+          onSubmit={handleProductSubmit}
+          onUpdate={handleProductUpdate}
+        />
       </Container>
     </div>
   );
